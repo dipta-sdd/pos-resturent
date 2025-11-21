f7dd8d7809244df10d18f7cf02cc22857612dfa1
dipta-sdd <sankarsandipta@gmail.com>
2025-11-21 22:35:46 +0600

    deleted react part

--- flavorfusion-restaurant-platform/client/app/customer/layout.tsx
+++
@@ -1,9 +0,0 @@
-import CustomerDashboardLayout from '../../layouts/CustomerDashboardLayout';
-
-export default function CustomerLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <CustomerDashboardLayout>{children}</CustomerDashboardLayout>;
-}
